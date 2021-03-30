856d7f69754733f58048aa7276ae38da6be11899
Saurabh Patel <saurabh.finch@gmail.com>
2021-03-30 17:31:34 +0530

    add babel plugin in webpack base config

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -140,6 +140,9 @@ module.exports = (env) => {
       'esper.js': 'esper'
     },
     plugins: [
+      "@babel/plugin-syntax-dynamic-import",
+      "@babel/plugin-proposal-object-rest-spread",
+      "@babel/plugin-proposal-optional-chaining",
       new webpack.ProgressPlugin({ profile: false }), // Always show build progress
       new ExtractTextPlugin({ // Move CSS into external file
         filename: 'stylesheets/[name].css'
